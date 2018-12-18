c8301d880747c50ddd7d640196a032814198f567
Robert Logan <rloganiv@gmail.com>
2018-12-17 16:26:42 -0800

    Perplexity and unkown penalized perplexity added

---
+++ kglm/training/metrics/perplexity.py
@@ -0,0 +1,128 @@
+"""
+Implementation of perplexity and unknown penalized perplexity metrics.
+"""
+from typing import Optional
+
+from allennlp.data.vocabulary import DEFAULT_OOV_TOKEN
+from allennlp.training.metrics import Metric
+from overrides import overrides
+import torch
+import torch.nn.functional as F
+
+from kglm.data.extended_vocabulary import ExtendedVocabulary
+
+
+@Metric.register('ppl')
+class Perplexity(Metric):
+    """Computes perplexity."""
+    def __init__(self) -> None:
+        self._sum_log_p = 0.0
+        self._total_count = 0.0
+
+    def __call__(self,
+                 logits: torch.Tensor,
+                 labels: torch.Tensor,
+                 mask: Optional[torch.Tensor]) -> None:
+        """
+        Parameters
+        ----------
+        logits : ``torch.Tensor``, required.
+            A tensor of class logits of shape (batch_size, k, sequence_length).
+        labels : ``torch.Tensor``, required.
+            A tensor of integer class labels of shape (batch_size, sequence_length).
+        mask: ``torch.Tensor``, optional (default = None).
+            A binary mask tensor of shape (batch_size, sequence_length).
+        """
+        logits, labels, mask = self.unwrap_to_tensors(logits, labels, mask)
+
+        log_p = F.cross_entropy(logits, labels, reduction='none')
+        if mask is not None:
+            self._sum_log_p += (mask * log_p).sum()
+            self._total_count += mask.sum()
+        else:
+            self._sum_log_p += log_p.sum()
+            self._total_count += torch.numel(labels)  # pylint: disable=no-member
+
+    @overrides
+    def get_metric(self, reset: bool) -> float:
+        ppl = self._sum_log_p / self._total_count
+        if reset:
+            self.reset()
+        return ppl
+
+    @overrides
+    def reset(self):
+        self._sum_log_p = 0.0
+        self._total_count = 0.0
+
+
+@Metric.register('upp')
+class UnknownPenalizedPerplexity(Metric):
+    """
+    Computes unknown penalized perplexity.
+
+    Parameters
+    ----------
+    vocabulary : ``ExtendedVocabulary``, required.
+        Vocabulary used by the model.
+    namespace : ``str``, optional(default = 'tokens')
+        Token namespace.
+    oov_token : ``str``, optional(default = '@@UNKNOWN@@')
+        Out of vocabulary token. AllenNLP's ``DEFAULT_OOV_TOKEN`` is used by default.
+    """
+    def __init__(self,
+                 vocabulary: ExtendedVocabulary,
+                 namespace: str,
+                 oov_token: str = DEFAULT_OOV_TOKEN) -> None:
+        # Compute the penalty weight applied to p(<unk>).
+        vocab_size = vocabulary.get_vocab_size(namespace)
+        unk_vocab_size = vocabulary.get_vocab_size(namespace + '_unk')
+        self._unk_penalty = -torch.log(1.0 + unk_vocab_size / vocab_size)  # pylint: disable=no-member
+
+        # Identify the index of the <unk> token.
+        self._unk_idx = vocabulary.get_token_index(oov_token, namespace=namespace)
+
+        # Initialize the metric variables.
+        self._sum_log_p = 0.0
+        self._total_count = 0.0
+
+    def __call__(self,
+                 logits: torch.Tensor,
+                 labels: torch.Tensor,
+                 mask: Optional[torch.Tensor]) -> None:
+        """
+        Parameters
+        ----------
+        logits : ``torch.Tensor``, required.
+            A tensor of class logits of shape (batch_size, k, sequence_length).
+        labels : ``torch.Tensor``, required.
+            A tensor of integer class labels of shape (batch_size, sequence_length).
+        mask: ``torch.Tensor``, optional (default = None).
+            A binary mask tensor of shape (batch_size, sequence_length).
+        """
+        logits, labels, mask = self.unwrap_to_tensors(logits, labels, mask)
+
+        log_p = F.cross_entropy(logits, labels, reduction='none')
+
+        # Apply penalty to unks
+        unk_ids = labels.eq(self._unk_idx)
+        log_p[unk_ids] += self._unk_penalty
+
+        if mask is not None:
+            self._sum_log_p += (mask * log_p).sum()
+            self._total_count += mask.sum()
+        else:
+            self._sum_log_p += log_p.sum()
+            self._total_count += torch.numel(labels)  # pylint: disable=no-member
+
+    @overrides
+    def get_metric(self, reset: bool) -> float:
+        ppl = self._sum_log_p / self._total_count
+        if reset:
+            self.reset()
+        return ppl
+
+    @overrides
+    def reset(self):
+        self._sum_log_p = 0.0
+        self._total_count = 0.0
