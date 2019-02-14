d087829712fca40e1a3728d750fb16ebff756110
Nelson Liu <nfliu@cs.washington.edu>
2019-02-14 12:52:19 -0800

    Fix awd_lstm_test

--- kglm/tests/models/awd_lstm_test.py
+++ kglm/tests/models/awd_lstm_test.py
@@ -1,14 +1,13 @@
 # pylint: disable=protected-access,not-callable,unused-import
-
-from allennlp.common.testing import ModelTestCase
 import numpy as np
 import torch
 
+from kglm.common.testing import KglmModelTestCase
 from kglm.data.dataset_readers.enhanced_wikitext import EnhancedWikitextReader
 from kglm.models.awd_lstm import AwdLstmLanguageModel
 
 
-class AwdLstmLanguageModelTest(ModelTestCase):
+class AwdLstmLanguageModelTest(KglmModelTestCase):
 
     def setUp(self):
         super().setUp()
@@ -16,6 +15,4 @@ class AwdLstmLanguageModelTest(ModelTestCase):
                           "kglm/tests/fixtures/enhanced-wikitext.jsonl")
 
     def test_model_can_train_save_and_load(self):
-        # We ignore the copy mode projection weights since not every sequence will involve a copy
-        # operation.
         self.ensure_model_can_train_save_and_load(self.param_file)
