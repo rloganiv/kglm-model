7e76080f7065777e3cdf5020abe494aa631fa3dd
Robert L. Logan IV <rloganiv@gmail.com>
2019-02-17 15:08:51 -0800

    INCOMPLETE: Added ``Kglm`` model skeleton + tests
    * The skeleton is just a duplicate of the ``AliasCopynet`` code w/
        additional parameters.
    
    * Added a tiny knowledge graph to the test fixtures.
    
    * The goal now is to layer in functionality while ensuring that the
        tests still pass.

---
+++ kglm/tests/models/kglm_test.py
@@ -0,0 +1,18 @@
+# pylint: disable=protected-access,not-callable,unused-import
+import numpy as np
+import torch
+
+from kglm.common.testing import KglmModelTestCase
+from kglm.data.dataset_readers.enhanced_wikitext import EnhancedWikitextKglmReader
+from kglm.models.kglm import Kglm
+
+
+class KglmTest(KglmModelTestCase):
+
+    def setUp(self):
+        super().setUp()
+        self.set_up_model("kglm/tests/fixtures/training_config/kglm.json",
+                          "kglm/tests/fixtures/enhanced-wikitext.jsonl")
+
+    def test_model_can_train_save_and_load(self):
+        self.ensure_model_can_train_save_and_load(self.param_file)
