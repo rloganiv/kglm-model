1f195f1b0d0e6560bfce7615d21f39589ad93764
rloganiv <rloganiv@gmail.com>
2019-02-21 12:56:57 -0800

    BUGFIX: Recent entities cannot occur in the present

--- kglm/tests/modules/test_recent_entities.py
+++ kglm/tests/modules/test_recent_entities.py
@@ -42,27 +42,28 @@ class RecentEntitiesTest(AllenNlpTestCase):
         # We know that the candidate ids are given in the order in the last test,
         # here we check that the mask looks correct for entities in the first batch.
         # The mask for entities 1 and 2 in the first sequence should be the same
-        assert candidate_mask[0,:,1].equal(candidate_mask[0,:,2])
-        # The mask for entity 3 should be 1 for the 2nd and 3rd timestep
-        expected_3 = torch.tensor([0, 1, 1], dtype=torch.uint8)
-        assert candidate_mask[0,:,3].equal(expected_3)
-        # The mask for entity 4 should only be 1 for the last timestep
-        expected_4 = torch.tensor([0, 0, 1], dtype=torch.uint8)
-        assert candidate_mask[0,:,4].equal(expected_4)
+        assert candidate_mask[0, :, 1].equal(candidate_mask[0,:,2])
+        # The mask for entity 3 should be 1 for the 3rd timestep - it is not recent when it is first observed
+        expected_3 = torch.tensor([0, 0, 1], dtype=torch.uint8)
+        assert candidate_mask[0, :, 3].equal(expected_3)
+        # The mask for entity 4 should only all zeros - it will not be recent until the next batch
+        expected_4 = torch.tensor([0, 0, 0], dtype=torch.uint8)
+        assert candidate_mask[0, :, 4].equal(expected_4)
 
         # Let's check that the remainders are correct
-        # Entity 4 should have a remainder of 1, and everything else should be filtered out.
-        assert self.recent_entities._remaining[0] == {4: 1}
+        # Entity 3 should have a remainder of 1, Entity 4 should have a remainder of 2, and
+        # everything else should be filtered out.
+        assert self.recent_entities._remaining[0] == {3: 1, 4: 2}
 
         # If we run on the same data again, the remainder for entity 4 should activate the mask for
-        # the first timestep.
+        # the first and second timestep.
         candidate_ids, candidate_mask = self.recent_entities(entity_ids_t0)
-        expected_4 = torch.tensor([1, 0, 1], dtype=torch.uint8)
-        assert candidate_mask[0,:,4].equal(expected_4)
+        expected_4 = torch.tensor([1, 1, 0], dtype=torch.uint8)
+        assert candidate_mask[0, :, 4].equal(expected_4)
 
-        # But - this should not happen if we reset the first sequence
+        # But this should not happen if we reset the first sequence
         reset = torch.tensor([1, 0], dtype=torch.uint8)
         self.recent_entities.reset(reset)
         candidate_ids, candidate_mask = self.recent_entities(entity_ids_t0)
-        expected_4 = torch.tensor([0, 0, 1], dtype=torch.uint8)
-        assert candidate_mask[0,:,4].equal(expected_4)
+        expected_4 = torch.tensor([0, 0, 0], dtype=torch.uint8)
+        assert candidate_mask[0, :, 4].equal(expected_4)
