504b366f0e9ce875ac654581fc29d9b5c54471e8
rloganiv <rloganiv@gmail.com>
2019-02-21 22:05:31 -0800

    Added no shortlist training option
    This forces model to predict on set of all entities.

--- kglm/tests/models/kglm_test.py
+++ kglm/tests/models/kglm_test.py
@@ -16,3 +16,13 @@ class KglmTest(KglmModelTestCase):
 
     def test_model_can_train_save_and_load(self):
         self.ensure_model_can_train_save_and_load(self.param_file)
+
+class KglmNoShortlistTest(KglmModelTestCase):
+
+    def setUp(self):
+        super().setUp()
+        self.set_up_model("kglm/tests/fixtures/training_config/kglm.no-shortlist.json",
+                          "kglm/tests/fixtures/enhanced-wikitext.jsonl")
+
+    def test_model_can_train_save_and_load(self):
+        self.ensure_model_can_train_save_and_load(self.param_file)
