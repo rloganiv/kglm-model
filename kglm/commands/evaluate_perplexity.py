fa335b22f6f8ce1636ca0bf9c98b8b6dd465af83
Nelson Liu <nfliu@cs.washington.edu>
2019-03-04 01:56:55 -0500

    Call iterator.eval

--- kglm/commands/evaluate_perplexity.py
+++ kglm/commands/evaluate_perplexity.py
@@ -148,6 +148,7 @@ def evaluate_from_args(args: argparse.Namespace) -> Dict[str, Any]:
     iterator_params = config.pop('iterator', 'None')
     iterator = DataIterator.from_params(iterator_params)
     iterator.index_with(model.vocab)
+    iterator.eval()
     metrics = evaluate_perplexity(model, sampler, args.num_samples,
                                   instances, iterator, args.cuda_device)
 
